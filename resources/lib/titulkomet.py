1d5a571e17428670070346d9a36c8f6ba8f0ae1b
Jastrab <jastrab.sk@gmail.com>
2022-01-30 11:17:19 +0100

    - Kodi 19 - fix prehravania videí (jastrab) - pridaná možnosť prehratia cez originál youtube plugin  (jastrab) - fix na Kodi 19.3 Matrix (jastrab) - migracia settings pre Kodi 19 (jastrab)

--- resources/lib/titulkomet.py
+++ resources/lib/titulkomet.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 # /*
 # *      Copyright (C) 2017 BrozikCZ
+# *                    2022 Jastrab      
 # *
 # *
 # *  This Program is free software; you can redistribute it and/or modify
@@ -22,26 +23,49 @@
 
 import re
 import urllib
-import urllib2
-import cookielib
-from xml.etree.ElementTree import fromstring
+# import urllib2
+# import cookielib
+
+# try:
+#     from urllib2 import Request, urlopen, HTTPError
+#     import cookielib
+# except:
+# from urllib.request import Request, urlopen
+# from urllib.error import HTTPError
+# import http.cookiejar as cookielib
+
+
+#Python 2
+try: 
+    import cookielib
+    import urllib2
+    import sys
+    reload(sys)  # Reload does the trick!
+    sys.setdefaultencoding('UTF8')
+#Python 3
+except:
+    import http.cookiejar
+    cookielib = http.cookiejar
+    urllib2 = urllib.request
+
+# from xml.etree.ElementTree import fromstring
 from demjson import demjson
 
 import util
-import resolver
-from provider import ResolveException
-from provider import ContentProvider
+# import resolver
+# from provider import ResolveException
+# from provider import ContentProvider
+from contentprovider.provider import ResolveException
+from contentprovider.provider import ContentProvider
 import YDStreamExtractor
-
 
 class TitulkometContentProvider(ContentProvider):
     def __init__(self, username=None, password=None, filter=None,
-                 tmp_dir='/tmp', quality=3):
+                 tmp_dir='/tmp'):
         ContentProvider.__init__(self, 'titulkomet.cz',
                                  'http://www.titulkomet.cz',
                                  username, password, filter, tmp_dir)
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
-        self.quality = int(quality)
         urllib2.install_opener(opener)
 
     def capabilities(self):
@@ -54,7 +78,7 @@ class TitulkometContentProvider(ContentProvider):
             return self.list_content(util.request(self._url(url)), self._url(url))
 
     def search(self, keyword):
-        return self.list('/?submit=Search&s=' + urllib.quote(keyword))
+        return self.list('/?submit=Search&s=' + urllib2.quote(keyword))
 
     def categories(self):
         result = []
@@ -82,7 +106,6 @@ class TitulkometContentProvider(ContentProvider):
         data = util.substr(page, '<main id=\"main', '<div id=\"secondary')
         pattern = '<article id=.+?<a href=\"(?P<url>[^\"]+)\" *title=\"(?P<title>[^\"]+)\".+?<img width=.+? src=\"(?P<img>[^\"]+)\".+?hodnocení: <strong>(?P<rating>[^<]+?)</strong>.+?<p>(?P<plot>.+?)<\/p>.'
         for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
-            print "item"
             item = self.video_item()
             item['title'] = self.format_title(m)
             item['img'] = m.group('img').strip()
@@ -123,10 +146,10 @@ class TitulkometContentProvider(ContentProvider):
         return result
 
     def format_title(self, m):
-    	return "{0} - {1}%".format(m.group('title'), int(float(m.group('rating').replace(",", ".")) / 5 * 100))
+        return "{0} - {1}%".format(m.group('title'), int(float(m.group('rating').replace(",", ".")) / 5 * 100))
 
     def decode_plot(self, m):
-    	p = m.group('plot')
+        p = m.group('plot')
         p = re.sub('<br[^>]*>', '', p)
         p = re.sub('<div[^>]+>', '', p)
         p = re.sub('<table.*', '', p)
@@ -149,16 +172,30 @@ class TitulkometContentProvider(ContentProvider):
         resolved = []
         item = item.copy()
         url = self._url(item['url'])
+        self.info('== resolve titulkomet ===>' + url)
+        original_yt = False
+
+        
+
+
         data = util.substr(util.request(url), 'jQuery( document ).ready(function()', '</script>')
-        print 'data start ----'
-        print data
-        print 'data end ----'
 
         urls = re.findall('file:[ ]+\"(?P<url>[^\"].+?)\"', data, re.IGNORECASE | re.DOTALL | re.MULTILINE)
-        vid = YDStreamExtractor.getVideoInfo(urls[0], quality=self.quality) #quality is 0=SD, 1=720p, 2=1080p, 3=Highest Available
+        self.info(urls)
+        if original_yt:
+            url2 = urls[0]
+            # e = 'watch?v='
+            e = 'youtu.be/'
+            edx = url2.find(e)
+            video_id = url2[edx+len(e):]
+
+       # video_url = resolver.findstreams([urls[0].replace('https://youtu.be/', 'https://www.youtube.com/watch?v=')])
+        vid = YDStreamExtractor.getVideoInfo(url, quality=3) #quality is 0=SD, 1=720p, 2=1080p, 3=Highest Available
         video_url = [vid.streams()[0]]
         subs = urls[1]
-
+        # self.info(video_url)
+        
+        self.info(subs)
         if video_url and subs:
             for i in video_url:
                 i['subs'] = subs
@@ -168,21 +205,27 @@ class TitulkometContentProvider(ContentProvider):
             raise ResolveException('Video nenalezeno')
 
         for i in resolved:
-           item = self.video_item()
-           try:
-               item['title'] = i['title']
-           except KeyError:
-               pass
-           item['url'] = i['xbmc_url']
-           item['quality'] = i['ytdl_format']['height']
-           item['surl'] = i['ytdl_format']['webpage_url']
-           item['subs'] = i['subs']
-           item['headers'] = {}
-           try:
-               item['fmt'] = i['fmt']
-           except KeyError:
-               pass
-           result.append(item)
+            item = self.video_item()
+            try:
+                item['title'] = i['title']
+            except KeyError:
+                pass
+            item['url'] = i['xbmc_url']# i['url']
+            if original_yt:
+                item['url'] = "plugin://plugin.video.youtube/?action=play_video&videoid=" + video_id
+                
+            #item['quality'] = i['quality']
+            #item['surl'] = i['surl']
+            item['quality'] = i['ytdl_format']['height']
+            item['surl'] = i['ytdl_format']['webpage_url']
+            item['subs'] = i['subs']
+            item['headers'] = {}#i['headers']
+            self.info(item)
+            try:
+                item['fmt'] = i['fmt']
+            except KeyError:
+                pass
+            result.append(item)
             
         return result
 
